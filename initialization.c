68c593dc130be314b5773208dc0d22f9c665d086
bugi <bugi@bugi-Aspire-1810TZ>
2013-01-12 21:20:21 +0200

    Muutettu hitulistaan

--- initialization.c
+++ initialization.c
@@ -1,45 +1,71 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include <gsl/gsl_rng.h>
 
-int init_cordinates(int *grid, int num_particles, int length) {
-    if (num_particles >= length * length) {
-        printf("Too many particles to fit the box!");
-    }
-    int i;
-    for (i = 0; i < length * length; i++) {
-        grid[i] = 0;
-    }
-    gsl_rng * rand_gen = gsl_rng_alloc(gsl_rng_mt19937);
-    /* Generate random positions for the particles */
-    for (i = 0; i < num_particles; i++) {
-        int x = gsl_rng_uniform_int (rand_gen, length);
-        int y = gsl_rng_uniform_int (rand_gen, length);
-        if (grid[y * length + x]) { /* Particle already in the selected position. */
-            i--;
-            continue;
-        }
-        grid[y * length + x] = 1;
-    }
-    return 0;
+/* Particles are represented by its x,y-cordinates.  */
+typedef struct partic {
+    int x, y, z;
+    int v_x, v_y, v_z;
+} particle;
+
+/* Define length of a side in the simulation grid in Å. Sizes are the ones asked to be used. */
+/* Cast to int to make things easier */
+int gas_box_size(int N){
+    return (int)(33.38 * cbrt(N));
+}
+int liquid_box_size(int N){
+    return (int)(3.62 * cbrt(N));
 }
 
-void print_grid(int *grid, int L) {
+void init_particles(particle *particles, int num_particles, int length) {
     int i, j;
-    for (i = 0; i < L; i++) {
-        for (j = 0; j < L; j++) {
-            printf("%u ", grid[i * L + j]);
+    gsl_rng *rand_gen = gsl_rng_alloc(gsl_rng_mt19937);
+    for (i = 0; i < num_particles; i++) {
+        /* Generate x and y for a particle */
+        int rand1 = gsl_rng_uniform_int (rand_gen, length);
+        int rand2 = gsl_rng_uniform_int (rand_gen, length);
+        int rand3 = gsl_rng_uniform_int (rand_gen, length);
+        /* Check if a particle with these coordinates already exists */
+        int found = 0;
+        for (j = 0; j < i; j++) {
+            if (particles[j].x == rand1 && particles[i].y == rand2 && particles[i].z == rand3) {
+                i--;
+                found = 1;
+                break;
+            }
         }
-        putchar('\n');
+        if (!found) {
+            particle new;
+            new.x = rand1;
+            new.y = rand2;
+            new.z = rand3;
+//            new.x = rand1;
+//            new.y = rand2;
+            particles[i] = new;
+        }
     }
-    putchar('\n');
+}
+
+void init_gas_particles(particle *particles, int num_particles) {
+    init_particles(particles, num_particles, gas_box_size(num_particles));
+}
+
+void init_liquid_particles(particle *particles, int num_particles) {
+    init_particles(particles, num_particles, liquid_box_size(num_particles));
+}
+
+double potential_energy(int *grid) {
+    return 0.0;
 }
 
 int main(void) {
-    
+    int i;
     int N = 10;
-    int L = 10;
-    int grid[L*L];
-    init_cordinates(grid, N, L);
-    print_grid(grid, L);
+    particle *particles = (particle *)calloc(N, sizeof(particle));
+    init_gas_particles(particles, N);
+    for (i = 0; i < N; i++) {
+        printf("%d %d\n", particles[i].x, particles[i].y);
+    }
+    return 0;
 }
